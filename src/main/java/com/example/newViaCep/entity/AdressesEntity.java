0aa378b02faeaffb2686fa06ea9232c046652b91
NcryScoobycls <fabricio_c_anelli@hotmail.com>
2021-08-20 17:14:57 -0300

    Adicionando AdressesEntity

---
+++ src/main/java/com/example/newViaCep/entity/AdressesEntity.java
@@ -0,0 +1,46 @@
+package com.example.newViaCep.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AdressesEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String publicPlace;
+	private String zipCode;
+	
+	
+	public AdressesEntity(){
+		
+	}
+	
+	
+	public String getPublicPlace() {
+		return publicPlace;
+	}
+	public void setPublicPlace(String publicPlace) {
+		this.publicPlace = publicPlace;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+
+	@Override
+	public String toString() {
+		return "AdressesEntity [id=" + id + ", publicPlace=" + publicPlace + ", zipCode=" + zipCode + "]";
+	}
+	
+	
+	
+	
+
+}
