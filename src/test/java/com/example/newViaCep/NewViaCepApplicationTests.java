4014448acc0a59358b00acc605f46087321cab52
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-08-20 16:59:17 -0300

    add Project

---
+++ src/test/java/com/example/newViaCep/NewViaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newViaCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewViaCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
